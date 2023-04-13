f21ddb2d9278e9988b09443247b95e811e49010b
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-04-12 18:53:40 -0700

    orbitting experimenting

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   useEffect(() => {
     const scene = new THREE.Scene();
 
-    scene.background = new THREE.Color(0x4A302B);
+    scene.background = new THREE.Color(0x000000);
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -16,54 +16,60 @@ export default function Home() {
 
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.position.setZ(40);
+    camera.position.setZ(70);
     camera.position.setX(-2);
+    camera.position.setY(-1);
 
     renderer.render(scene, camera);
 
-    const geometry = new THREE.SphereGeometry(10, 32, 32);
-    const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const sphere1Texture = new THREE.TextureLoader().load('mars-texture.jpg');
+
+    const geometry = new THREE.SphereGeometry(1, 0, 0);
+    const material = new THREE.MeshStandardMaterial({ 
+      map: sphere1Texture,
+    });
     const sphere = new THREE.Mesh(geometry, material);
 
-    const geometry2 = new THREE.SphereGeometry(3, 40, 40);
-    const material2 = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const geometry2 = new THREE.SphereGeometry(14, 40, 40);
+    const material2 = new THREE.MeshStandardMaterial({ color: 0x8C8C8C });
     const sphere2 = new THREE.Mesh(geometry2, material2);
-    sphere2.position.z = 20;
-    sphere2.position.x = -10;
+    sphere2.position.z = -20;
+    sphere2.position.x = -33;
+    sphere2.position.y = 0;
 
-    const geometry3 = new THREE.SphereGeometry(4, 40, 40);
-    const material3 = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const geometry3 = new THREE.SphereGeometry(14, 40, 40);
+    const material3 = new THREE.MeshStandardMaterial({ color: 0x8C8C8C });
     const sphere3 = new THREE.Mesh(geometry3, material3);
-    sphere3.position.z = 0;
-    sphere3.position.x = 20;
+    sphere3.position.z = 33;
+    sphere3.position.x = 0;
     sphere3.position.y = 0;
 
-    const geometry4 = new THREE.SphereGeometry(3, 40, 40);
-    const material4 = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const geometry4 = new THREE.SphereGeometry(14, 40, 40);
+    const material4 = new THREE.MeshStandardMaterial({ color: 0x8C8C8C});
     const sphere4 = new THREE.Mesh(geometry4, material4);
-    sphere4.position.z = 2;
-    sphere4.position.x = -20;
+    sphere4.position.z = -20;
+    sphere4.position.x = 33;
     sphere4.position.y = 0;
 
     scene.add(sphere, sphere2, sphere3, sphere4);
 
     sphere.add(sphere2, sphere3, sphere4);
     sphere.rotation.x = Math.PI / 5;
+    sphere.rotation.y = Math.PI / 5;
 
 
     const pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(10, 10, 10);
+    pointLight.position.set(0, 0, 70);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff);
-    scene.add(pointLight, ambientLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    scene.add(pointLight);
 
     function animate() {
       requestAnimationFrame(animate);
     
-      sphere.rotation.y += 0.012;
-      // sphere.rotation.x += 0.01;
-
-    
+      sphere.rotation.y += 0.005;
+      sphere2.rotation.x += 0.01;
+  
       // controls.update();
     
       renderer.render(scene, camera);
